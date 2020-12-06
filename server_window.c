a2770b209439767c21fc6e8a02c30e6dda060676
PageOfPages <51131231+PageOfPages@users.noreply.github.com>
2020-12-06 18:46:45 -0500

    Create server_window.c

---
+++ server_window.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int server(int user_ID, char pwd[30])
+{
+    FILE *infile;
+
+    if ((infile = fopen("clients.txt", "r")) == NULL) {
+        puts("File could not be accessed");
+    }
+    else 
+    {
+        int ID;
+        char password[30];
+        char first[30];
+        char last[30];
+        double credit;
+        int user_found = 0;
+
+        fscanf(infile,"%d%29s%29s%29s%lf", &ID, first, last, password, &credit);
+
+        while(!feof(infile) ){
+            if (ID == user_ID & (strcmp(password, pwd) == 0)) {
+                user_found = 1;
+                if (credit == 0) {
+                    puts("You have no credit remaining in your account.");
+                }
+                else {
+                    printf("Hello, %s %s! You are now in the system.\n", first, last);
+                    printf("Your Current Credit is $%.2f\n", credit);
+                }
+            }
+            fscanf(infile,"%d%29s%29s%29s%lf", &ID, first, last, password, &credit);
+        }
+        if (user_found == 0) {
+            puts("Sorry, unfortunately you do not have an account with us.");
+        }
+        fclose(infile); 
+    }
+}
