3d278fd1d932b6780316695fb735b67bd8a4e985
ninjagirl3691 <55964142+ninjagirl3691@users.noreply.github.com>
2020-12-08 12:09:27 -0500

    updated to work with temp_server

--- temp_client.c
+++ temp_client.c
@@ -1,6 +1,7 @@
 //Work in progress - Jazz
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #include "server_window.c"
 
 
@@ -8,23 +9,41 @@ int main(void) {
     char user_type;
     
     puts("Welcome to our server!");
-    printf("Are you an Admin or User? ");
+    printf("Are you an Admin(A) or User?(U) ");
     scanf("%s", &user_type);
+    user_type = tolower(user_type);
     
-    if (user_type == "User" || user_type == "user"){
-        int user_ID;
-        char pwd[30];
-        printf("Enter your account ID number: ");
-        scanf("%d", &user_ID);
-        printf("Enter your password: ");
-        scanf("%s", pwd);
+    if (user_type == 'u'){
+        char client;
+        
+        printf("Are you a currently existing client? [Y/N] ");
+        scanf("%s", &client);
+        client = tolower(client);
+        
+        if (client == 'y') {
+            int user_ID;
+            char pwd[30];
+            printf("Enter your account ID number: ");
+            scanf("%d", &user_ID);
+            printf("Enter your password: ");
+            scanf("%s", pwd);
     
-        server(user_ID, pwd);
+            server(user_ID, pwd);
+        }
+        
+        if (client == 'n') {
+            puts("add user WIP");
+//            add_user();
+        }
+        
     }
     
-    if (user_type=="Admin" || user_type=="admin") {
-        //char pwd[30];
+    if (user_type=='a') {
+        char apwd[30];
         
-        admin();
+        printf("Enter the admin password for this server: ");
+        scanf("%s", apwd);
+        
+        admin(apwd);
     }
 }
